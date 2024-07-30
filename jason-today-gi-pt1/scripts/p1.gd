de19c4564bf2bc2cbc11492cda241f13a9bb943d
Joe Booth <joe@joebooth.com>
2024-07-29 23:22:13 -0700

    seperate draw shader

--- jason-today-gi-pt1/scripts/p1.gd
+++ jason-today-gi-pt1/scripts/p1.gd
@@ -6,11 +6,19 @@ extends TextureRect
 # @export var clear_screen: int = 0
 # @export_range(0, 5, .1) var merge_fix: int = 4
 
+
+@export var color = Vector3(1.,1.,0)
+@export var from = Vector2(100,100)
+@export var to = Vector2(300,200)
+@export_range(1, 15) var radius:float = 1.
+@export  var drawing: bool = true
+
 #@onready var ui_output: Label = $CanvasLayer/MarginContainer/HBoxContainer/Panel/VBoxContainer/output
 
 
 var shader_file_names = {
-	"pt1": "res://shaders/pt1.glsl",
+	#"pt1": "res://shaders/pt1.glsl",
+	"draw": "res://shaders/draw.glsl",
 }
 
 var rd: RenderingDevice
@@ -22,6 +30,7 @@ var consts_buffer
 
 var output_texture
 var output_tex_uniform
+var input_tex_uniform
 
 @export var texture_rect: TextureRect
 
@@ -58,6 +67,10 @@ func setup():
 	output_tex_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
 	output_tex_uniform.binding = 1
 	output_tex_uniform.add_id(output_texture)
+	input_tex_uniform = RDUniform.new()
+	input_tex_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+	input_tex_uniform.binding = 2
+	input_tex_uniform.add_id(output_texture)
 
 	for key in shader_file_names.keys():
 		var file_name = shader_file_names[key]
@@ -75,7 +88,7 @@ func simulate(delta:float):
 	# n/a
 
 	#--- GPU work
-	pt1()
+	draw()
 
 	# GPU -> CPU
 	rd.submit()
@@ -101,6 +114,27 @@ func send_image():
 	var byte_data : PackedByteArray = rd.texture_get_data(output_texture, 0)
 	var image_data := Image.create_from_data(size.x, size.y, false, Image.FORMAT_RGBAF, byte_data)
 	texture.update(image_data)
+
+
+func draw():
+	var radiusSquared:float = radius * radius;
+	var pc_bytes := PackedFloat32Array([radiusSquared]).to_byte_array()
+	pc_bytes.append_array(PackedInt32Array([drawing,]).to_byte_array())
+	pc_bytes.append_array(PackedVector2Array([from, to, size]).to_byte_array())
+	pc_bytes.append_array(PackedVector3Array([color]).to_byte_array())
+	pc_bytes.resize(ceil(pc_bytes.size() / 16.0) * 16)
+
+	var shader_name = "draw"
+	var consts_buffer_uniform = get_uniform(consts_buffer, 0)
+	var uniform_set = rd.uniform_set_create([
+		consts_buffer_uniform, output_tex_uniform, input_tex_uniform,
+		], 
+		shaders[shader_name], 
+		0)
+
+	var compute_list = rd.compute_list_begin()
+	dispatch(compute_list, shader_name, uniform_set, pc_bytes)
+	rd.compute_list_end()
 
 func  pt1():
 	var pc_bytes := PackedInt32Array([
--- jason-today-gi-pt1/shaders/pt1.glsl
+++ jason-today-gi-pt1/shaders/pt1.glsl
@@ -1,57 +1,101 @@
 #[compute]
 #version 450
 
-layout(local_size_x = 16, local_size_y = 16) in;
+layout(local_size_x=16,local_size_y=16)in;
 
-layout(set = 0, binding = 0, std430) buffer ConstBuffer {
+layout(set=0,binding=0,std430)buffer ConstBuffer{
     uint add_data;
-} consts;
+}consts;
 
 // layout(set = 0, binding = 1, rgba32f) writeonly uniform image2D output_image;
-layout(set = 0, binding = 1, rgba32f) uniform image2D output_image;
+layout(set=0,binding=1,rgba32f)uniform image2D output_image;
 
+layout(push_constant,std430)uniform Params{
+    int rayCount;
+    int maxSteps;
+    bool showNoise;
+    bool accumRadiance;
+    vec2 size;
+}pc;
 
-layout(push_constant, std430) uniform Params {
-    uint add_data;
-} pc;
+vec2 vUv;
 
-// Draw a line shape!
-float sdfLineSquared(vec2 p, vec2 from, vec2 to) {
-  vec2 toStart = p - from;
-  vec2 line = to - from;
-  float lineLengthSquared = dot(line, line);
-  float t = clamp(dot(toStart, line) / lineLengthSquared, 0.0, 1.0);
-  vec2 closestVector = toStart - line * t;
-  return dot(closestVector, closestVector);
+const float PI = 3.14159265;
+const float TAU = 2.0 * PI;
+
+float rand(vec2 co) {
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
 }
 
 
-void main() {
-    vec2 fragCoord = gl_GlobalInvocationID.xy;
-    ivec2 ifragCoord = ivec2(fragCoord.xy);
 
-    ivec2 screen_size = imageSize(output_image).xy;
-    vec4 fragColor = vec4(ifragCoord.x / float(screen_size.x), ifragCoord.y / float(screen_size.y), 0.0, 1.0);
+bool outOfBounds(vec2 uv){
+    return uv.x<0.||uv.x>1.||uv.y<0.||uv.y>1.;
+}
 
-    // vec4 current = texture(inputTexture, vUv);
-    vec4 current = imageLoad(output_image, ifragCoord);
-    vec2 from = vec2(25, 25);
-    vec2 to = vec2(75, 75);
-    float radius = 5.0;
-    float radiusSquared = radius * radius;
-    // vec3 color = fragColor.rgb;
-    vec3 color = vec3(1.0, 1.0, 0.0);
-
-    // If we aren't actively drawing (or on mobile) no-op!
-    if (true) {
-        // vec2 coord = vUv * resolution;
-        vec2 coord = fragCoord;
-        if (sdfLineSquared(coord, from, to) <= radiusSquared) {
-        current = vec4(color, 1.0);
+vec4 raymarch(){
+    vec4 light=texture(sceneTexture,vUv);
+    
+    if(light.a>.1){
+        return light;
+    }
+    
+    float oneOverRayCount=1./float(rayCount);
+    float tauOverRayCount=TAU*oneOverRayCount;
+    
+    // Distinct random value for every pixel
+    float noise=rand(vUv);
+    
+    vec4 radiance=vec4(0.);
+    
+    for(int i=0;i<rayCount;i++){
+        float angle=tauOverRayCount+(float(i)+noise);
+        vec2 rayDirectionUv=vec2(cos(angle),-sin(angle))/size;
+        
+        // Our current position, plus one step.
+        vec2 sampleUv=vUv+rayDirectionUv;
+        
+        for(int step=0;step<maxSteps;step++){
+            if(outOfBounds(sampleUv))break;
+            
+            vec4 sampleLight=texture(sceneTexture,sampleUv);
+            if(sampleLight.a>.1){
+                radiance+=sampleLight;
+                break;
+            }
+            
+            sampleUv+=rayDirectionUv;
+            
         }
     }
-    fragColor = current;
+    return radiance*oneOverRayCount;
+}
 
-
-    imageStore(output_image, ifragCoord, fragColor);
+void main(){
+    vec2 fragCoord=gl_GlobalInvocationID.xy;
+    ivec2 ifragCoord=ivec2(fragCoord.xy);
+    
+    ivec2 screen_size=imageSize(output_image).xy;
+    vec4 fragColor=vec4(ifragCoord.x/float(screen_size.x),ifragCoord.y/float(screen_size.y),0.,1.);
+    
+    // vec4 current = texture(inputTexture, vUv);
+    vec4 current=imageLoad(output_image,ifragCoord);
+    vec2 from=vec2(25,25);
+    vec2 to=vec2(75,75);
+    float radius=5.;
+    float radiusSquared=radius*radius;
+    // vec3 color = fragColor.rgb;
+    vec3 color=vec3(1.,1.,0.);
+    
+    // If we aren't actively drawing (or on mobile) no-op!
+    if(true){
+        // vec2 coord = vUv * resolution;
+        vec2 coord=fragCoord;
+        if(sdfLineSquared(coord,from,to)<=radiusSquared){
+            current=vec4(color,1.);
+        }
+    }
+    fragColor=current;
+    
+    imageStore(output_image,ifragCoord,fragColor);
 }
